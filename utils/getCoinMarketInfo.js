9eb654c053a69391eee36e47eccf1b2ece4fe99e
ianyshuang <ianyshuang@gmail.com>
2021-06-13 22:21:04 +0800

    feat: add "/utils/getCoinMarketInfo" to speed up getting coin market information from coingecko

---
+++ utils/getCoinMarketInfo.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch')
+const MARKET_URL = 'https://api.coingecko.com/api/v3/coins/markets'
+
+module.exports = async () => {
+  const coinListItem = await getCoinList()
+  const { coinIdList } = coinListItem
+
+  const coinPerReq = 400
+  const iteration = Math.ceil(coinIdList.length / coinPerReq)
+  const promises = []
+    for (let i = 0; i < iteration; i++) {
+      let start = i * coinPerReq;
+      let end = Math.min((i + 1) * coinPerReq, coinIdList.length)
+      let ids = coinIdList.slice(start, end)
+      let idString = ids.join('%2C')
+      console.log(`fetching ${start} ~ ${end}`)
+      
+      // set the range of coins to fetch
+      let queryString = `vs_currency=usd&ids=${idString}&order=market_cap_desc&sparkline=false`
+
+      // fetch data and concat to "coinPriceList"
+      let response = fetch(`${MARKET_URL}?${queryString}`)
+      promises.push(response)
+  }
+
+  const responses = await Promise.all(promises)
+    .then(values => values)
+    .catch(error => console.log(error))
+
+  return responses
+}
