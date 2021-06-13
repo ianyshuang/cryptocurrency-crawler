0834f5091691ac92795514603cd4f04b03edee6f
ianyshuang <ianyshuang@gmail.com>
2021-06-13 22:21:59 +0800

    feat: add "createCoinPriceDay()" & "updateCoinPriceDay()" to create / update item in CoinPriceDay table

--- index.js
+++ index.js
@@ -11,155 +11,51 @@ const dynamoClient = new AWS.DynamoDB.DocumentClient({
   accessKeyId: process.env.DYNAMO_KEY,
   secretAccessKey: process.env.DYNAMO_SECRET
 })
+global.dynamoClient = dynamoClient
 
-// 從 Coingecko 爬取資料並更新 "CoinList" 這個 Table
-const updateCoinList = async (url) => {
-  // 從 Coingecko 取得當前最新的 coin list
-  const response = await fetch(url)
-  const data = await response.json()
-  const coinIdList = data.map(d => d.id)
+// these methods need to be required after dynamoClient put into "global"
+const { getCoinList } = require('./getter/coinList')
+const { updateCoinList } = require('./setter/coinList')
+const { updateCoinMarket } = require('./setter/coinMarket')
+const { createCoinPriceDay, updateCoinPriceDay } = require('./setter/coinPrice')
 
-  // 更新 "CoinUpdateTime" 這個 Table
-  console.time('CoinUpdateTime')
-  let time = new Date().getTime()
-  // 在 "CoinUpdateTime" 中，將 "CoinList" 最新版本的時間 append 到 "datetime" 這個 field 中
-  await dynamoClient.update({
-    TableName: 'CoinUpdateTime',
-    Key: { table: 'CoinList' },
-    UpdateExpression: 'SET #datetime = list_append(#datetime, :time)',
-    ExpressionAttributeNames: { '#datetime': 'datetime' },
-    ExpressionAttributeValues: { ':time': [time] }
-  }).promise()
-    .then(() => console.log('succcessfully updated CoinUpdateTime') )
-    .catch(error => {
-      console.log('fail to update CoinUpdateTime')
-      console.log(error)
-    })
-  console.timeEnd('CoinUpdateTime')
 
-  // 新增 item 到 "CoinList" 中
-  await dynamoClient.put({
-    TableName: 'CoinList',
-    Item: {
-      datetime: time,  // primary key
-      coins: coinIdList
-    }
-  }).promise()
-    .then(() => console.log('successfully updated CoinList'))
-    .catch(error => {
-      console.log('fail to update CoinList')
-      console.log(error)
-    })
-}
+const getCoinMarketInfo = async (url) => {
+  const coinListItem = await getCoinList()
+  const { coinIdList } = coinListItem
 
-// 從 Dynamo 取得最新的 CoinList
-const getCoinList = async () => {
-  // 從 CoinUpdateTime 中取得 CoinList 目前最新版本的 datetime (primary key)
-  const coinUpdateTime = await dynamoClient.get({
-    TableName: 'CoinUpdateTime',
-    Key: { table: 'CoinList' }
-  }).promise()
-  const timeSeries = coinUpdateTime.Item.datetime
-  const latestTime = timeSeries[timeSeries.length - 1]
+  const coinPerReq = 400
+  const iteration = Math.ceil(coinIdList.length / coinPerReq)
+  const promises = []
+    for (let i = 0; i < iteration; i++) {
+      let start = i * coinPerReq;
+      let end = Math.min((i + 1) * coinPerReq, coinIdList.length)
+      let ids = coinIdList.slice(start, end)
+      let idString = ids.join('%2C')
+      console.log(`fetching ${start} ~ ${end}`)
+      
+      // set the range of coins to fetch
+      let queryString = `vs_currency=usd&ids=${idString}&order=market_cap_desc&sparkline=false`
 
-  const data = await dynamoClient.get({
-    TableName: 'CoinList',
-    Key: { datetime: latestTime }
-  }).promise()
-
-  return data.Item.coins
-}
-
-// 從 Coingecko 爬取資料並更新 "CoinMarket" 這個 Table
-const updateCoinMarket = async (url) => {
-  // get coin list from database
-  console.time('get coinList')
-  const coinList = await getCoinList()
-  console.timeEnd('get coinList')
-  
-  // 從 coinList 中每次取出 250 個 coins (API limit)
-  // 向 coingecko 發 requests 取得 coin market 資料
-  console.time('fetch data')
-  const iteration = Math.ceil(coinList.length / 250)
-  let coinMarketList = []
-  for (let i = 0; i < iteration; i++) {
-    let start = i * 250;
-    let end = Math.min((i + 1) * 250, coinList.length)
-    let ids = coinList.slice(start, end)
-    let idString = ids.join('%2C')
-    console.log(`fetching ${start} ~ ${end}`)
-    
-    // set the range of coins to fetch
-    let queryString = `vs_currency=usd&&order=market_cap_desc&sparkline=false&ids=${idString}&price_change_percentage=1h%2C24h%2C7d`
-
-    // fetch data and concat to "coinMarketList"
-    let response = await fetch(`${url}?${queryString}`)
-    let data = await response.json()
-    coinMarketList = coinMarketList.concat(data)
-  }
-  console.timeEnd('fetch data')
-
-  // sort data with "market_cap" field in descending order
-  console.time('sorting')
-  coinMarketList.sort((prev, next) => next.market_cap - prev.market_cap)
-  console.timeEnd('sorting')
-
-  // 更新 "CoinUpdateTime"
-  // apppend 最新版本的時間到 CoinMarket 的 "datetime"
-  console.time('update coinUpdateTime')
-  const now = new Date()
-  const time = now.getTime()
-  
-  await dynamoClient.update({
-    TableName: 'CoinUpdateTime',
-    Key: { table: 'CoinMarket' },
-    UpdateExpression: 'SET #datetime = list_append(#datetime, :time)',
-    ExpressionAttributeNames: { '#datetime': 'datetime' },
-    ExpressionAttributeValues: { ':time': [time] }
-  }).promise()
-    .then(() => console.log('successfully update CoinUpdateTime') )
-    .catch(error => {
-      console.log('fail to update CoinUpdateTime')
-      console.log(error)
-    })
-  console.timeEnd('update coinUpdateTime')
-
-  // 更新 "CoinMarket" (by page)
-  const itemData = []
-  const pages = Math.ceil(coinMarketList.length / 100)
-  for (let i = 0; i < pages; i++) {
-    let start = i * 100
-    let end = Math.min((i + 1) * 100, coinMarketList.length)
-    itemData.push({
-      datetime: time,
-      page: i + 1,
-      coins: coinMarketList.slice(start, end)
-    })
+      // fetch data and concat to "coinPriceList"
+      let response = fetch(`${url}?${queryString}`)
+      promises.push(response)
   }
 
-  for (let i = 0; i < itemData.length; i++) {
-    dynamoClient.put({
-      TableName: 'CoinMarket',
-      Item: itemData[i]
-    }).promise()
-      .then(() => {
-        console.log(`successfully write CoinMarket item page ${i+1}`)
-      })
-      .catch(error => {
-        console.log(`fail to write CoinMarket item page ${i+1}`)
-        console.log(error)
-      })
-  }
+  const responses = await Promise.all(promises)
+    .then(values => values)
+    .catch(error => console.log(error))
+
+  return responses
 }
-
-
 
 const run = async() => {
-  const coinListUrl = 'https://api.coingecko.com/api/v3/coins/list'
-  const coinMarketUrl = 'https://api.coingecko.com/api/v3/coins/markets'
-  
   // updateCoinList(coinListUrl)
   // updateCoinMarket(coinMarketUrl)
+  
+  // createCoinPriceDay()
+  // updateCoinPriceDay()
+
 }
 
 
---
+++ setter/coinPrice.js
@@ -0,0 +1,114 @@
+const { getCoinList } = require('../getter/coinList')
+const getCoinMarketInfo = require('../utils/getCoinMarketInfo')
+const SEG_SIZE = 30
+
+// 建立 CoinPriceDay 的 items
+const createCoinPriceDay = async () => {
+  const { dynamoClient } = global
+  
+  // 取得 coinList 資料
+  const coinListItem = await getCoinList()
+  const { coinIdList, coinPriceSegment } = coinListItem
+
+  // 計算 12 am / 12 pm 的 timestamp
+  const now = new Date()
+  now.setHours(0, 0, 0, 0)
+  const dateStartTime = now.getTime()
+  now.setHours(12, 0, 0, 0)
+  const dateMidTime = now.getTime()
+
+  // 建立每個 segment 的 item
+  const segCounts = Math.ceil(coinIdList.length / SEG_SIZE)
+  const itemList = []
+  for (let i = 1; i <= segCounts; i++) {
+    itemList.push({ segment: i, coinPrice: {} })
+  }
+  for (let coin in coinPriceSegment) {
+    let segment = coinPriceSegment[coin]
+    itemList[segment - 1]['coinPrice'][coin] = []
+  }
+  
+  for (let item of itemList) {
+    dynamoClient.put({
+      TableName: 'CoinPriceDay',
+      Item: {
+        ...item,
+        time: dateStartTime
+      }
+    }).promise()
+      .then(() => console.log(`successfully create segment ${item['segment']} - 1`))
+      .catch(error => console.log(error))
+
+    dynamoClient.put({
+      TableName: 'CoinPriceDay',
+      Item: {
+        ...item,
+        time: dateMidTime
+      }
+    }).promise()
+      .then(() => console.log(`successfully create segment ${item['segment']} - 2`))
+      .catch(error => console.log(error))
+  }
+}
+
+// 從 Coingecko 爬取資料並更新 "CoinPriceFiveMin" 這個 Table
+const updateCoinPriceDay = async () => {
+  const { dynamoClient } = global
+  const coinListItem = await getCoinList()
+
+  // 設定時間
+  const now = new Date()
+  const nowTime = now.getTime()
+  now.setHours(0, 0, 0, 0)
+  const dateStartTime = now.getTime()
+  now.setHours(12, 0, 0, 0)
+  const dateMidTime = now.getTime()
+  const time = nowTime >= dateMidTime ? dateMidTime : dateStartTime
+  
+  // 從 coingecko 爬資料
+  console.time('fetch coin market')
+  const responses = await getCoinMarketInfo()
+  console.timeEnd('fetch coin market')
+  const newCoinPrice = {}
+  for (let res of responses) {
+    let data = await res.json()
+    data.forEach(d => {
+      newCoinPrice[d.id] = {
+        current_price: d.current_price,
+        last_updated: d.last_updated
+      }
+    })
+  }
+
+  // // 每 30 個 coins 為一個 segment
+  const segCounts = Math.ceil(coinListItem.coinIdList.length / SEG_SIZE)
+  for (let i = 1; i <= segCounts; i++) {
+    let data = await dynamoClient.get({
+      TableName: 'CoinPriceDay',
+      Key: { segment: i, time: time }
+    }).promise()
+    
+    let item = data.Item
+    let oldCoinPrice = item.coinPrice
+    for (let coin in oldCoinPrice) {
+      oldCoinPrice[coin].push(newCoinPrice[coin])
+    }
+
+    dynamoClient.update({
+      TableName: 'CoinPriceDay',
+      Key: { segment: i, time: time },
+      UpdateExpression: 'SET coinPrice = :coinPrice',
+      ExpressionAttributeValues: { ':coinPrice': oldCoinPrice }
+    }).promise()
+      .then(() => console.log(`successfully update CoinPriceDay segment ${i}`))
+      .catch(error => {
+        console.log(`fail to update CoinPriceDay segment ${i}`)
+        console.log(error)
+      })
+  }
+}
+
+module.exports = {
+  createCoinPriceDay,
+  updateCoinPriceDay
+}
