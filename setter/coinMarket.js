bc86f19496ce5bf7edf88bc8450e92ad5b9ac73f
ianyshuang <ianyshuang@gmail.com>
2021-06-14 18:49:51 +0800

    feat: modify "updateCoinMarket()", use "getCoinMarketInfo()" to speed up requests to coingecko, change item's field due to change of table structure

--- setter/coinMarket.js
+++ setter/coinMarket.js
@@ -1,36 +1,17 @@
-const fetch = require('node-fetch')
-const { getCoinList } = require('../getter/coinList')
-const { dynamoClient } = global
-const MARKET_URL = 'https://api.coingecko.com/api/v3/coins/markets'
+const getCoinMarketInfo = require('../utils/getCoinMarketInfo')
 
 // 從 Coingecko 爬取資料並更新 "CoinMarket" 這個 Table
 const updateCoinMarket = async () => {
-  // get coin list from database
-  console.time('get coinList')
-  const coinList = await getCoinList()
-  console.timeEnd('get coinList')
-  
-  // 從 coinList 中每次取出 250 個 coins (API limit)
-  // 向 coingecko 發 requests 取得 coin market 資料
-  console.time('fetch data')
-  const iteration = Math.ceil(coinList.length / 250)
-  let coinMarketList = []
-  for (let i = 0; i < iteration; i++) {
-    let start = i * 250;
-    let end = Math.min((i + 1) * 250, coinList.length)
-    let ids = coinList.slice(start, end)
-    let idString = ids.join('%2C')
-    console.log(`fetching ${start} ~ ${end}`)
-    
-    // set the range of coins to fetch
-    let queryString = `vs_currency=usd&order=market_cap_desc&sparkline=false&ids=${idString}&price_change_percentage=1h%2C24h%2C7d`
+  console.time('fetch coin market')
+  const responses = await getCoinMarketInfo()
+  console.timeEnd('fetch coin market')
 
-    // fetch data and concat to "coinMarketList"
-    let response = await fetch(`${MARKET_URL}?${queryString}`)
-    let data = await response.json()
+  let coinMarketList = []
+  for (let res of responses) {
+    let data = await res.json()
     coinMarketList = coinMarketList.concat(data)
   }
-  console.timeEnd('fetch data')
+
 
   // sort data with "market_cap" field in descending order
   console.time('sorting')
@@ -39,7 +20,8 @@ const updateCoinMarket = async () => {
 
   // 更新 "CoinMarket" (by page)
   const now = new Date()
-  const time = now.getTime()
+  const fiveMin = 1000 * 60 * 5 // 5 minutes in milliseconds
+  const time = Math.round(now.getTime() / fiveMin) * fiveMin  // nearest XX:X0 / XX:X5 's timestamp
   const itemData = []
   const pages = Math.ceil(coinMarketList.length / 100)
   for (let i = 0; i < pages; i++) {
@@ -48,7 +30,7 @@ const updateCoinMarket = async () => {
     itemData.push({
       time: time,
       page: i + 1,
-      coins: coinMarketList.slice(start, end)
+      coinMarket: coinMarketList.slice(start, end)
     })
   }
 
